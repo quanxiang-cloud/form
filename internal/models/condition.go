1faf7375c760a34e0b4abd610b71b6db9458897f
hejiaming <jameshe@yunify.com>
2022-03-04 15:18:05 +0800

    feature:implement method Value & Scan

--- internal/models/condition.go
+++ internal/models/condition.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/quanxiang-cloud/form/internal/service/types"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"github.com/quanxiang-cloud/form/internal/service/types"
+)
 
 type Condition struct {
 	Bool BOOL `json:"bool,omitempty"`
@@ -9,4 +13,14 @@ type Condition struct {
 type BOOL struct {
 	Must   types.Entities `json:"must,omitempty"`
 	Should types.Entities `json:"should,omitempty"`
+}
+
+// Value 实现方法
+func (c *Condition) Value() (driver.Value, error) {
+	return json.Marshal(c)
+}
+
+// Scan 实现方法
+func (c *Condition) Scan(data interface{}) error {
+	return json.Unmarshal(data.([]byte), &c)
 }
