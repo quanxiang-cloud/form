55b90a106db78a913b51f949c0487101e0ddaf45
zhouhuiting <huitingzhou@yunify.com>
2022-03-31 17:49:50 +0800

    feature:change router

--- api/form/router.go
+++ api/form/router.go
@@ -52,10 +52,10 @@ func NewRouter(c *config2.Config) (*Router, error) {
 	}
 
 	r := map[string]*gin.RouterGroup{
-		managerPath:  engine.Group("/api/v1/form1/:appID/m"),
-		homePath:     engine.Group("/api/v1/form1/:appID/home"),
-		v2HomePath:   engine.Group("/api/v2/form1/:appID/home"),
-		internalPath: engineInner.Group("/api/v1/form1/:appID/internal"),
+		managerPath:  engine.Group("/api/v1/form/:appID/m"),
+		homePath:     engine.Group("/api/v1/form/:appID/home"),
+		v2HomePath:   engine.Group("/api/v2/form/:appID/home"),
+		internalPath: engineInner.Group("/api/v1/form/:appID/internal"),
 	}
 	for _, f := range routers {
 		err = f(c, r)
